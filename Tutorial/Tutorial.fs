5430f2ab2a61ab535106a08b7eae5d2b0d63cb16
Roland Tritsch <roland@tritsch.org>
2015-05-03 14:21:16 +0100

    Tutorial: Module: Units

--- Tutorial/Tutorial.fs
+++ Tutorial/Tutorial.fs
@@ -669,34 +669,26 @@ end
 // ---------------------------------------------------------------
 //         Units of measure
 // ---------------------------------------------------------------
-
-module UnitsOfMeasure =
+module UnitsOfMeasure = begin
     // Code can be annotated with units of measure when using F# arithmetic over numeric types
-
     open Microsoft.FSharp.Data.UnitSystems.SI.UnitNames
 
     [<Measure>]
-    type mile =
-
+    type mile = class
         /// Conversion factor mile to meter: meter is defined in SI.UnitNames
-
         static member asMeter = 1600.<meter/mile>
+    end
 
- 
     let d  = 50.<mile>          // Distance expressed using imperial units
     let d' = d * mile.asMeter   // Same distance expressed using metric system
 
     printfn "%A = %A" d d'
     // let error = d + d'       // Compile error: units of measure do not match
-
-
- 
+end
 
 // ---------------------------------------------------------------
 //         Parallel array programming
 // ---------------------------------------------------------------
- 
-
 module ParallelArrayProgramming =
 
     let oneBigArray = [| 0 .. 100000 |]

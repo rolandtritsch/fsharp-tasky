07cdddee24d10d8908f678b62eec0cf65df4885a
Roland Tritsch <roland@tritsch.org>
2015-05-03 11:47:59 +0100

    Tutorial: Module: Array

--- Tutorial/Tutorial.fs
+++ Tutorial/Tutorial.fs
@@ -338,11 +338,7 @@ end
 // ---------------------------------------------------------------
 //         Arrays
 // ---------------------------------------------------------------
-
-
-module Arrays =
-
-
+module Arrays = begin
     /// The empty array
     let array1 = [| |]
 
@@ -351,36 +347,39 @@ module Arrays =
     let array3 = [| 1 .. 1000 |]
 
     /// An array containing only the words "hello" and "world"
+    let array4 = [| 
+        for word in array2 do
+            if word.Contains("l") then yield word 
+        done
+    |]
 
-    let array4 = [| for word in array2 do
-                        if word.Contains("l") then
-                            yield word |]
-
-
-    /// An array initialized by index and containing the even numbers from 0 to 2000
-    let evenNumbers = Array.init 1001 (fun n -> n * 2)
+    /// An array initialized by index and containing the even numbers from 0 to 1998
+    let evenNumbers = Array.init 1000 (fun n -> n * 2)
+    let evenNumbers2 = array3 |> Array.filter(fun i -> (i%2 = 0))
 
     /// sub-array extracted using slicing notation
-    let evenNumbersSlice = evenNumbers.[0..500]
+    let evenNumbersSlice = evenNumbers.[0 .. 499]
 
     for word in array4 do
         printfn "word: %s" word
+    done
 
     // modify an array element using the left arrow assignment operator
     array2.[1] <- "WORLD!"
 
     /// Calculates the sum of the lengths of the words that start with 'h'
-    let sumOfLengthsOfWords =
+    let sumOfLengthsOfWords = begin
         array2
         |> Array.filter (fun x -> x.StartsWith "h")
         |> Array.sumBy (fun x -> x.Length)
- 
+    end
+
+    let tail = array2.[1..]
+end
 
 // ---------------------------------------------------------------
 //         Sequences
 // ---------------------------------------------------------------
-
- 
 module Sequences =
 
     // Sequences are evaluated on-demand and are re-evaluated each time they are iterated.

2d76e7455b94b784ddca76026b162290d331e95a
Roland Tritsch <roland@tritsch.org>
2015-05-03 11:53:37 +0100

    Tutorial: Module: Sequences

--- Tutorial/Tutorial.fs
+++ Tutorial/Tutorial.fs
@@ -380,43 +380,51 @@ end
 // ---------------------------------------------------------------
 //         Sequences
 // ---------------------------------------------------------------
-module Sequences =
-
+module Sequences = begin
     // Sequences are evaluated on-demand and are re-evaluated each time they are iterated.
     // An F# sequence is an instance of a System.Collections.Generic.IEnumerable<'T>,
     // so Seq functions can be applied to Lists and Arrays as well.
 
     /// The empty sequence
     let seq1 = Seq.empty
-
-    let seq2 = seq { yield "hello"; yield "world"; yield "and"; yield "hello"; yield "world"; yield "again" }
+    let seq2 = seq { 
+        yield "hello"
+        yield "world"
+        yield "and"
+        yield "hello"
+        yield "world"
+        yield "again" 
+    }
 
     let numbersSeq = seq { 1 .. 1000 }
 
     /// another array containing only the words "hello" and "world"
-    let seq3 =
-        seq { for word in seq2 do
-                  if word.Contains("l") then
-                      yield word }
+    let seq3 = seq { 
+        for word in seq2 do 
+            if word.Contains("l") then yield word 
+        done
+    }
 
-
-    let evenNumbers = Seq.init 1001 (fun n -> n * 2)
+    let evenNumbers = Seq.init 1000 (fun n -> n * 2)
 
     let rnd = System.Random()
 
     /// An infinite sequence which is a random walk
     //  Use yield! to return each element of a subsequence, similar to IEnumerable.SelectMany.
 
-    let rec randomWalk x =
-        seq { yield x
-              yield! randomWalk (x + rnd.NextDouble() - 0.5) }
+    let rec randomWalk x = begin
+        seq { 
+            yield x
+            yield! randomWalk (x + rnd.NextDouble() - 0.5) 
+        }
+    end
  
-
-    let first100ValuesOfRandomWalk =
+    let first100ValuesOfRandomWalk = begin
         randomWalk 5.0
         |> Seq.truncate 100
         |> Seq.toList
-
+    end
+end
 
 // ---------------------------------------------------------------
 //         Recursive functions
